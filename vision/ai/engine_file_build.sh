d4884ef8fe0aaa076c858adb3f77e308e6379087
debanik <debanik@meraque.ai>
2026-07-01 15:48:51 +0800

    # ./engine_file_build.sh mardi_pineapple_yolo_v8 n 3

--- vision/ai/engine_file_build.sh
+++ vision/ai/engine_file_build.sh
@@ -3,6 +3,7 @@
 # Usage: ./engine_file_build.sh [model_name] [model_type] [num_classes]
 # Defaults: model_name=yolov8n, model_type=n, num_classes=80
 # Example: ./engine_file_build.sh mardi_pineapple_yolo_v8 n 3
+# ./engine_file_build.sh mardi_pineapple_yolo_v8 n 3
 
 MODEL_NAME="${1:-yolov8n}"
 MODEL_TYPE="${2:-n}"
