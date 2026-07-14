839242d1175f90f0117b61580092c8c933ef64f0
debanik <debanik@meraque.ai>
2026-07-14 14:57:50 +0800

    !/*.py

---
+++ vision/ai/yolov8_trt_bed_detect_orin/intel_vid_stream.py
@@ -0,0 +1,26 @@
+import cv2
+
+camera = cv2.VideoCapture("/dev/video2", cv2.CAP_V4L2)
+
+if not camera.isOpened():
+    print("Could not open camera")
+    raise SystemExit(1)
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+camera.set(cv2.CAP_PROP_FPS, 30)
+
+while True:
+    ret, frame = camera.read()
+
+    if not ret:
+        print("Failed to read frame")
+        break
+
+    cv2.imshow("RealSense D435i", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
