30ee2ee5628fd700d18eb3240904991531df4f54
debanik <debanik@meraque.ai>
2026-07-14 10:56:04 +0800

    sudo docker info | grep -i runtime

--- install_docker_jetson_orin_nano.sh
+++ install_docker_jetson_orin_nano.sh
@@ -78,6 +78,19 @@ sudo docker run --rm \
 
 cat <<'EOF'
 
+
+ldconfig -p | grep libcuda
+/usr/local/cuda/bin/nvcc --version
+ls /dev/nvhost-ctrl /dev/nvmap
+dpkg -l | grep -i tensorrt
+dpkg-query -W nvidia-jetpack
+apt-cache policy nvidia-jetpack
+cat /etc/nv_tegra_release
+dpkg-query -W -f='${Version}\n' nvidia-jetpack
+
+sudo docker info | grep -i runtime
+
+
 Installation completed.
 
 To use Docker without sudo, either:
