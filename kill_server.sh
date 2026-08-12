f58d2a3d5edff4d2f55177e665dc5bad0c759292
debanik <debanik@meraque.ai>
2026-08-12 15:34:45 +0800

    docker rm -f $(docker ps -aq)

--- kill_server.sh
+++ kill_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # xhost +local:
 # xhost +local:
-docker rm -f $(docker ps -aq)
+# docker rm -f $(docker ps -aq)
 # docker compose down
 # docker compose -f docker-compose.rviz2.yaml down
 
 # sudo chown -R $USER:$USER ${HOME}/ZED_BAG
 # sudo chown -R $USER:$USER ${HOME}/yolo_models
-# docker compose down
+docker compose down
 # docker compose -f docker-compose.prime.yaml down
