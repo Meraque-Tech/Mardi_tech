8ffb710e116079efba688693c2bc7b18fa7a9adf
debanik <debanik@meraque.ai>
2026-08-12 15:07:09 +0800

    testing

--- run_ai.sh
+++ run_ai.sh
@@ -5,8 +5,8 @@ ARCH=$(uname -m)
 case "$ARCH" in
   x86_64)
     ./kill_server.sh
-    # docker compose up -d yolov8-trt-bed-detect-x86-jazzy
-    docker compose up -d yolov8-trt-bed-detect-x86-jazzy --build
+    docker compose build yolov8-trt-bed-detect-x86-jazzy
+    docker compose up -d yolov8-trt-bed-detect-x86-jazzy
     docker compose up -d imu_gnss_raw
     docker compose logs -f imu_gnss_raw
     ;;
