a53f163e7f3575c30b8235d6ad7b4aa023e29ec0
debanik <debanik@meraque.ai>
2026-08-12 14:44:48 +0800

    Refactor network interface configuration in cyclone_dds_profile.xml and update Docker commands in run_ai.sh for clarity

--- run_ai.sh
+++ run_ai.sh
@@ -4,12 +4,14 @@ ARCH=$(uname -m)
 
 case "$ARCH" in
   x86_64)
-    docker compose up -d yolov8-trt-bed-detect-x86-jazzy --build
-    # docker compose up -d yolov8-trt-bed-detect-x86-jazzy
+    docker compose up -d yolov8-trt-bed-detect-x86-jazzy
+    # docker compose up -d yolov8-trt-bed-detect-x86-jazzy --build
+    docker compose up imu_gnss_raw
+    
     ;;
   aarch64)
     docker compose up -d imu_gnss_raw yolov8-trt-bed-detect-orin
-    docker compose up imu_gnss_raw
+    
     ;;
   *)
     echo "Unsupported architecture: $ARCH"
