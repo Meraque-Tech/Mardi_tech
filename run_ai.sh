93d3d79608babced536518b222212604686004e0
debanik <debanik@meraque.ai>
2026-08-12 14:31:08 +0800

    docker compose up -d yolov8-trt-bed-detect-x86-jazzy --build

--- run_ai.sh
+++ run_ai.sh
@@ -4,11 +4,11 @@ ARCH=$(uname -m)
 
 case "$ARCH" in
   x86_64)
-    docker compose up -d yolov8-trt-bed-detect-x86-jazzy
+    docker compose up -d yolov8-trt-bed-detect-x86-jazzy --build
+    # docker compose up -d yolov8-trt-bed-detect-x86-jazzy
     ;;
   aarch64)
-    docker compose up -d imu_gnss_raw yolov8-trt-bed-detect-orin --build
-    # docker compose up -d imu_gnss_raw yolov8-trt-bed-detect-orin
+    docker compose up -d imu_gnss_raw yolov8-trt-bed-detect-orin
     ;;
   *)
     echo "Unsupported architecture: $ARCH"
