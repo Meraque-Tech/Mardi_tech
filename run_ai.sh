28e0a17fa39532cb989d3e6eff6edf5253ba935e
debanik <debanik@meraque.ai>
2026-08-13 10:11:10 +0800

    Comment out build and push commands for aarch64 architecture in run_ai.sh

--- run_ai.sh
+++ run_ai.sh
@@ -15,10 +15,10 @@ case "$ARCH" in
     ;;
   aarch64)
     ./kill_server.sh
-    docker compose build imu_gnss_raw-aarch64 \
-          yolov8-trt-bed-detect-orin \
-          sensor_data_raw-aarch64
-    docker compose push imu_gnss_raw-aarch64 yolov8-trt-bed-detect-orin sensor_data_raw-aarch64
+    # docker compose build imu_gnss_raw-aarch64 \
+    #       yolov8-trt-bed-detect-orin \
+    #       sensor_data_raw-aarch64
+    # docker compose push imu_gnss_raw-aarch64 yolov8-trt-bed-detect-orin sensor_data_raw-aarch64
     docker compose up -d imu_gnss_raw-aarch64 \
           yolov8-trt-bed-detect-orin \
           sensor_data_raw-aarch64
