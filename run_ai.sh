30d8dc8c59aa97a9cdeb1effb1835a0766412954
debanik <debanik@meraque.ai>
2026-08-13 09:01:42 +0800

    Fix run_ai.sh to remove sensor_data_raw-x86 from docker compose up command for x86 architecture

--- run_ai.sh
+++ run_ai.sh
@@ -7,7 +7,8 @@ case "$ARCH" in
     ./kill_server.sh
     docker compose build imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
     # docker compose push imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy
-    docker compose up -d imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
+    docker compose up -d imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy
+    # docker compose up -d imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
 
     docker compose logs -f imu_gnss_raw-x86
 
