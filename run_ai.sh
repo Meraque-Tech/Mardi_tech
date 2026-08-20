daecbfccd00c6c463241a7d672bc60d51d942f57
debanik <debanik@meraque.ai>
2026-08-20 17:21:51 +0800

    docker compose push imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86

--- run_ai.sh
+++ run_ai.sh
@@ -6,8 +6,8 @@ case "$ARCH" in
   x86_64)
     ./kill_server.sh
     docker compose build imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
-    # docker compose push imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
-    docker compose up -d imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy
+    docker compose push imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
+    # docker compose up -d imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy
     # docker compose up -d imu_gnss_raw-x86 yolov8-trt-bed-detect-x86-jazzy sensor_data_raw-x86
 
     # docker compose logs -f imu_gnss_raw-x86
